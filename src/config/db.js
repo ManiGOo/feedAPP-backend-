7102e2e8747e65bb2147678b93de257aecd23722
Manish <its.nathmanish@gmail.com>
2025-08-29 23:39:54 +0530

    preview commit

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Neon SSL
+  },
 });
 
 export default pool;
